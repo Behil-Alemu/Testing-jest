dc76b01995873de46974ebc3dd532c6a97e01dc0
behilalemu <91044960+Behil-Alemu@users.noreply.github.com>
2022-10-18 22:59:41 -0400

    finish markov.js

--- makeText.js
+++ makeText.js
@@ -1,2 +1,39 @@
 /** Command-line tool to generate Markov text. */
 
+const fs = require("fs");
+const markov = require("./markov");
+const axios = require("axios");
+const process = require("process");
+
+function generateText(text) {
+    let mm = new markov.MarkovMachine(text);
+    console.log(mm.makeText());
+  }
+
+function makeText(path){
+    fs.readFile(path, "utf8", function(err, file) {
+      if (err) {
+        console.error("You done messed up",err);
+        process.exit(1);
+      }else{
+        generateText(file);  
+      }
+    });
+    }
+
+async function webUrl(url){
+        try {
+        let result = await axios.get(url) 
+        console.log(result.data)
+        }catch (error) {
+            console.error(`You done messed up, URL ${url} not found`,error);
+            process.exit(1);
+        }
+    
+    }
+let path = process.argv[2]
+if (path.slice(0,4) === "http"){
+    webUrl(process.argv[2])
+}else{
+    makeText(process.argv[2])
+}
--- markov.js
+++ markov.js
@@ -17,13 +17,42 @@ class MarkovMachine {
    *  {"the": ["cat", "hat"], "cat": ["in"], "in": ["the"], "hat": [null]} */
 
   makeChains() {
-    // TODO
-  }
-
+    let chains = new Map();
+    for (let i = 0; i < this.words.length; i += 1){
+      let currWord = this.words[i];
+      let nextWord = this.words[i + 1] || null;
+      if(chains.has(currWord))
+        chains.get(currWord).push(nextWord)
+      else chains.set(currWord, [nextWord])
+      }
+      this.chains = chains
+  
+    }
 
   /** return random text from chains */
+  static choice(list){
+    let randIdx = Math.floor(Math.random() * list.length)
+    return list[randIdx]
+  }
 
   makeText(numWords = 100) {
-    // TODO
+    let keys = Array.from(this.chains.keys());
+    let key = MarkovMachine.choice(keys);
+    let out = [];
+
+    // produce markov chain until reaching termination word
+    while (out.length < numWords && key !== null) {
+      out.push(key);
+      key = MarkovMachine.choice(this.chains.get(key));
+    }
+
+    return out.join(" ");
   }
 }
+
+// let mm = new MarkovMachine("the cat in the hat");
+// console.log(mm.makeText())
+
+module.exports = {
+  MarkovMachine,
+};
